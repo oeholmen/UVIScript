2655bb1abaea0398b87d2414be15ede2e586f5f0
Øystein Holmen <oeholmen@icloud.com>
2023-01-02 19:57:16 +0100

    Refactor to local functions

--- includes/common.lua
+++ includes/common.lua
@@ -1,7 +1,7 @@
 --------------------------------------------------------------------------------
 -- Common methods
 --------------------------------------------------------------------------------
-  
+
 local function getRandom(min, max, factor)
   if type(min) == "number" and type(max) == "number" and min < max then
     return math.random(min, max)
@@ -36,64 +36,69 @@ local function getIndexFromValue(value, selection)
   return nil
 end
 
-return {  
-  getRandom = getRandom,
-  getRandomBoolean = getRandomBoolean,
-  getChangeMax = getChangeMax,
-  getIndexFromValue = getIndexFromValue,
-
-  randomizeValue = function(value, limitMin, limitMax, randomizationAmount)
-    if randomizationAmount > 0 then
-      local limitRange = limitMax - limitMin
-      local changeMax = getChangeMax(limitRange, randomizationAmount)
-      local min = math.max(limitMin, (value - changeMax))
-      local max = math.min(limitMax, (value + changeMax))
-      --print("Before randomize value", value)
-      value = getRandom(min, max)
-      --print("After randomize value/changeMax/min/max", value, changeMax, min, max)
-    end
-    return value
-  end,
-  
-  tableIncludes = function(theTable, theItem)
-    return type(getIndexFromValue(theItem, theTable)) == "number"
-  end,
-  
-  round = function(value)
-    local int, frac = math.modf(value)
-    --print("int/frac", int, frac)
-    if math.abs(frac) < 0.5 then
-      value = int
-    elseif value < 0 then
-      value = int - 1
-    else
-      value = int + 1
-    end
-    return value
-  end,
-  
-  getRandomFromTable = function(theTable, except)
-    if #theTable == 0 then
-      return nil
-    end
-    if #theTable == 1 then
-      return theTable[1]
-    end
-    local index = getRandom(#theTable)
-    local value = theTable[index]
-    --print("getRandomFromTable index, value", index, value)
-    if type(except) ~= "nil" then
-      local maxRounds = 10
-      while value == except and maxRounds > 0 do
-        value = theTable[getRandom(#theTable)]
-        maxRounds = maxRounds - 1
-        --print("getRandomFromTable except, maxRounds", except, maxRounds)
-      end
-    end
-    return value
-  end,
-  
-  trimStartAndEnd = function(s)
-    return s:match("^%s*(.-)%s*$")
+local function randomizeValue(value, limitMin, limitMax, randomizationAmount)
+  if randomizationAmount > 0 then
+    local limitRange = limitMax - limitMin
+    local changeMax = getChangeMax(limitRange, randomizationAmount)
+    local min = math.max(limitMin, (value - changeMax))
+    local max = math.min(limitMax, (value + changeMax))
+    --print("Before randomize value", value)
+    value = getRandom(min, max)
+    --print("After randomize value/changeMax/min/max", value, changeMax, min, max)
   end
+  return value
+end
+
+local function round(value)
+  local int, frac = math.modf(value)
+  --print("int/frac", int, frac)
+  if math.abs(frac) < 0.5 then
+    value = int
+  elseif value < 0 then
+    value = int - 1
+  else
+    value = int + 1
+  end
+  return value
+end
+
+local function tableIncludes(theTable, theItem)
+  return type(getIndexFromValue(theItem, theTable)) == "number"
+end
+
+local function getRandomFromTable(theTable, except)
+  if #theTable == 0 then
+    return nil
+  end
+  if #theTable == 1 then
+    return theTable[1]
+  end
+  local index = getRandom(#theTable)
+  local value = theTable[index]
+  --print("getRandomFromTable index, value", index, value)
+  if type(except) ~= "nil" then
+    local maxRounds = 10
+    while value == except and maxRounds > 0 do
+      value = theTable[getRandom(#theTable)]
+      maxRounds = maxRounds - 1
+      --print("getRandomFromTable except, maxRounds", except, maxRounds)
+    end
+  end
+  return value
+end
+
+local function trimStartAndEnd(s)
+  return s:match("^%s*(.-)%s*$")
+end
+
+return {  
+  round = round,
+  getRandom = getRandom,
+  getChangeMax = getChangeMax,
+  tableIncludes = tableIncludes,
+  randomizeValue = randomizeValue,
+  trimStartAndEnd = trimStartAndEnd,
+  getRandomBoolean = getRandomBoolean,
+  getIndexFromValue = getIndexFromValue,
+  getRandomFromTable = getRandomFromTable,
 }
